6b99e0cfaf99821501a715a0a9e2057794eb1a4c
Angelina Ebreo <angelinaebreo@gmail.com>
2021-06-29 12:42:34 -0400

    complete lab

--- regifter.sql
+++ regifter.sql
@@ -15,27 +15,28 @@ CREATE DATABASE regifter;
 -- giver - string
 -- value - integer
 -- previously_regifted boolean
-
+CREATE TABLE gifts ( id serial, gift TEXT, giver TEXT, value INT, previously_regifted BOOLEAN );
 
 -- 
 \echo See details of the table you created
 -- 
-
+\d gifts;
 
 -- 
 \echo Alter the table so that the column price is changed to value 
 -- 
-
+ALTER TABLE gifts RENAME price TO value;
+-- I wrote the command although we created column value in our 1st step
 
 -- 
 \echo Insert a peach candle, given by 'Santa' thats value is 9 and has been previously regifted
 -- 
-
+INSERT INTO gifts (gift, giver, value, previously_regifted) VALUES ('peach candle', 'Santa', 9, TRUE);
 
 --
 \echo Query for all the columns in your gifts table
 -- 
-
+SELECT * FROM gifts;
 
 --
 \echo Uncomment below to insert 5 more gifts
@@ -54,47 +55,60 @@ CREATE DATABASE regifter;
 \echo Insert 5 more gifts of your own choosing,  include 1 more candle
 --
 
+INSERT INTO gifts (gift, giver, value, previously_regifted)
+VALUES
+('lavender candle', 'Maria', 29, TRUE),
+('small mirror', 'Thomas', 14, FALSE),
+('huge painting', 'Peter', 199, TRUE),
+('a doll', 'creepy neighbor', 1, TRUE),
+('good chocolate', 'John', 26, FALSE)
+;
+
 
 
 --
 \echo Query for gifts with a price greater than or equal to 20
 --
-
+SELECT * FROM gifts WHERE value >= 20 ORDER BY value;
 
 --
 \echo Query for every gift that has the word candle in it, only show the gift column
 --
-
+SELECT gift FROM gifts WHERE gift ILIKE '%candle%';
 
 --
 \echo Query for every gift whose giver is Santa OR value is greater than 30
 --
+SELECT gift FROM gifts WHERE giver ILIKE '%santa%' OR value > 30;
 
 
 --
 \echo Query for every gift whose giver is NOT Santa
 --
+SELECT gift FROM gifts WHERE giver <> 'Santa';
 
 
 --
 \echo Update the second gift to have a value of 2999
 -- 
+UPDATE gifts SET value = 2999 WHERE id = 2;
+UPDATE gifts SET value = 2999 WHERE id = 2 RETURNING *;
 
 
 --
 \echo Query for the updated item
 --
-
+SELECT * FROM gifts WHERE id = 2;
 
 --
 \echo Delete all the gifts from Santa and return the 'value' and 'gift' of the gift you have deleted
 --
-
+DELETE FROM gifts WHERE giver = 'Santa' RETURNING gift, value;
 
 --
 \echo Query for all the columns in your gifts table one more time
 --
-
+SELECT * FROM gifts;
 
 
 -- BONUSES
@@ -102,16 +116,18 @@ CREATE DATABASE regifter;
 --
  \echo Count the total number of gifts that have the word candle in it
 -- 
-
+SELECT COUNT(gift) FROM gifts WHERE gift ILIKE '%candle%';
 
 --
 \echo Get the AVEREAGE value from all the gifts
 --
-
+SELECT AVG(value) FROM gifts;
 
 -- 
  \echo Limit to 3 gifts, offset by 2 and order by price descending
 --
+SELECT * FROM gifts ORDER BY value DESC OFFSET 2;
+
 
 --
 -- finish
