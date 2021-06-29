b75b1cfc629de1d71f07a63e7c54f5de0522d58d
Daniel Naranjo <MR.DANIEL.NARANJO@PROTONMAIL.COM>
2021-06-29 15:52:35 -0400

    Bonus completed

--- regifter.sql
+++ regifter.sql
@@ -110,17 +110,17 @@ SELECT * FROM gifts;
 --
  \echo Count the total number of gifts that have the word candle in it
 -- 
-
-
+SELECT COUNT(*) FROM gifts WHERE gift LIKE '%candle%';
 --
 \echo Get the AVEREAGE value from all the gifts
 --
-
+SELECT AVG(value) FROM gifts;
 
 -- 
  \echo Limit to 3 gifts, offset by 2 and order by price descending
 --
-
+SELECT gift FROM gifts ORDER BY value DESC LIMIT 3 OFFSET 2;
+SELECT * FROM gifts; 
 --
 -- finish
 --
