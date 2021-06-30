1ce3e27b381012349b45d58f77900b158b6b20b8
Jordan Bobadilla <jordanboba08@gmail.com>
2021-06-29 22:04:02 -0400

    complete bonus

--- regifter.sql
+++ regifter.sql
@@ -182,16 +182,37 @@ FROM
  \echo Count the total number of gifts that have the word candle in it
 -- 
 
+SELECT 
+    COUNT(gift)
+FROM
+    gifts
+WHERE 
+    gift ILIKE '%candle%'
+;
 
 --
 \echo Get the AVEREAGE value from all the gifts
 --
 
+SELECT 
+    AVG(value)
+FROM
+    gifts
+;
 
 -- 
  \echo Limit to 3 gifts, offset by 2 and order by price descending
 --
 
+SELECT
+    *
+FROM
+    gifts
+ORDER BY
+    value DESC
+LIMIT 
+    3 OFFSET 2
+;
 --
 -- finish
 --
