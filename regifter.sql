c31fdb306cbb2ebf50aff82991f6c5bd6f6f7136
jessicanetto <75263227+jessicanetto@users.noreply.github.com>
2021-06-29 21:51:42 -0400

    complete

--- regifter.sql
+++ regifter.sql
@@ -19,22 +19,23 @@ CREATE DATABASE regifter;
 
 -- 
 \echo See details of the table you created
--- 
+-- \dt gifts
 
 
 -- 
 \echo Alter the table so that the column price is changed to value 
--- 
+-- ALTER TABLE gifts RENAME value TO price;
 
 
 -- 
 \echo Insert a peach candle, given by 'Santa' thats value is 9 and has been previously regifted
--- 
+-- regifter=# INSERT INTO gifts (gift, giver, value, previously_regifted)
+-- regifter-# VALUES ('peach candle', 'Santa', '9', TRUE);
 
 
 --
 \echo Query for all the columns in your gifts table
--- 
+-- SELECT * FROM gifts
 
 
 --
@@ -43,57 +44,70 @@ CREATE DATABASE regifter;
 
 -- INSERT INTO gifts (gift, giver, value, previously_regifted)
 -- VALUES
--- ('peach candle', 'Santa', '9', TRUE),
--- ('cinnamon candle', 'Nick', '19', TRUE),
--- ('soap on a rope', 'Rudolf', '29', FALSE),
--- ('potpurri', 'Elf on the Shelf', '39', TRUE),
--- ('mango candle', 'The Boss', '49', FALSE)
--- ;
+('peach candle', 'Santa', '9', TRUE),
+('cinnamon candle', 'Nick', '19', TRUE),
+('soap on a rope', 'Rudolf', '29', FALSE),
+('potpurri', 'Elf on the Shelf', '39', TRUE),
+('mango candle', 'The Boss', '49', FALSE)
+;
 
--- 
+-- regifter=# INSERT INTO gifts (gift, giver, price, previously_regifted)
+regifter-# VALUES ('cinnamon candle', 'Nick', '19', TRUE),
+regifter-# ('soap on a rope', 'Rudolf', '29', FALSE),
+regifter-# ('potpurri', 'Elf on the Shelf', '39', TRUE),
+regifter-# ('mango candle', 'The Boss', '49', FALSE)
+regifter-# ;
+INSERT 0 4
 \echo Insert 5 more gifts of your own choosing,  include 1 more candle
---
+-- INSERT INTO gifts (gift, giver, price, previously_regifted)
+-- VALUES ('apple candle', 'Jess', '17', TRUE), 
+-- ('baseball', 'Pumpkin', '4', TRUE), 
+-- ('guitar', 'Dolly', '21', TRUE),
+-- ('shoes', 'Sunny', '3', FALSE),
+-- ('crayons', 'Ricky', '0', TRUE);
 
 
 
 --
 \echo Query for gifts with a price greater than or equal to 20
---
+--SELECT * FROM gifts WHERE price >= 20;
+
 
 
 --
 \echo Query for every gift that has the word candle in it, only show the gift column
---
+--SELECT * FROM gifts WHERE gift LIKE '%candle%';
 
 
 --
 \echo Query for every gift whose giver is Santa OR value is greater than 30
---
+--SELECT * FROM gifts WHERE giver = 'Santa' OR value > 20;
+
 
 
 --
 \echo Query for every gift whose giver is NOT Santa
---
+--SELECT * FROM gifts WHERE giver = !'Santa';
 
 
 --
 \echo Update the second gift to have a value of 2999
--- 
+-- UPDATE gifts SET value = 2999 WHERE id = 2;
 
 
 --
 \echo Query for the updated item
---
+--SELECT * FROM gifts WHERE id = 2;
 
 
 --
 \echo Delete all the gifts from Santa and return the 'value' and 'gift' of the gift you have deleted
---
+--DELETE FROM gifts WHERE giver = 'Santa' RETURNING value, gift;
 
 
 --
 \echo Query for all the columns in your gifts table one more time
---
+--SELECT * FROM gifts
 
 
 
