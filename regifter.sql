22227202ad3a542f085db6de63db8e890f20ffbe
Jordan Bobadilla <jordanboba08@gmail.com>
2021-06-29 21:43:25 -0400

    all queries possible done

--- regifter.sql
+++ regifter.sql
@@ -135,11 +135,25 @@ WHERE
 \echo Update the second gift to have a value of 2999
 -- 
 
+UPDATE
+    gifts
+SET
+    value = 2999
+WHERE
+    id = 2
+;
 
 --
 \echo Query for the updated item
 --
 
+SELECT
+    *
+FROM
+    gifts
+WHERE
+    id = 2 
+;
 
 --
 \echo Delete all the gifts from Santa and return the 'value' and 'gift' of the gift you have deleted
