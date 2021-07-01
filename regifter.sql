d1c9f01e3a40b1ce78121bd76b8b623ef7f39636
Alexander <alexanderl96@yahoo.com>
2021-06-30 21:15:57 -0400

    done

--- regifter.sql
+++ regifter.sql
@@ -15,7 +15,7 @@ CREATE DATABASE regifter;
 -- giver - string
 -- value - integer
 -- previously_regifted boolean
-CREATE TABLE gifts (id SERIAL PRIMARY KEY, gift TEXT, giver TEXT, price INT, previously_regifted BOOLEAN);
+CREATE TABLE gifts (id SERIAL PRIMARY KEY, gift TEXT, giver TEXT, value INT, previously_regifted BOOLEAN);
 
 -- 
 \echo See details of the table you created
@@ -31,7 +31,7 @@ ALTER TABLE gifts RENAME price TO value
 \echo Insert a peach candle, given by 'Santa' thats value is 9 and has been previously regifted
 -- 
 INSERT INTO gifts (gift, giver, value, previously_regifted)
-VALUES ('peach candle', 'Santa', '9', TRUE)
+VALUES ('peach candle', 'Santa', '9', TRUE);
 
 --
 \echo Query for all the columns in your gifts table
@@ -72,37 +72,37 @@ SELECT * FROM gifts WHERE value >= 20;
 \echo Query for every gift that has the word candle in it, only show the gift column
 --
 
-SELECT * FROM gifts WHERE gift = "candle";
+SELECT  gift FROM gifts WHERE gift ILIKE 'candle';
 --
 \echo Query for every gift whose giver is Santa OR value is greater than 30
 --
-SELECT * FROM gifts WHERE giver = "Santa" or value > 30;
+SELECT gift FROM gifts WHERE giver ILIKE "Santa" OR value > 30;
 
 --
 \echo Query for every gift whose giver is NOT Santa
 --
-SELECT * FROM gifts WHERE giver != "Santa";
+SELECT gift FROM gifts WHERE giver != "Santa";
 
 --
 \echo Update the second gift to have a value of 2999
 -- 
-UPDATE gifts SET value = 2999 WHERE id = 2;
+UPDATE gifts SET value = 2999 WHERE id = 2 RETURNING *;
 
 --
 \echo Query for the updated item
 --
+SELECT * FROM gifts WHERE id = 2;
 
-RETURNING *;
 --
 \echo Delete all the gifts from Santa and return the 'value' and 'gift' of the gift you have deleted
 --
-
+DELETE FROM gifts WHERE giver = 'Santa' RETURNING gift, value;
 
 --
 \echo Query for all the columns in your gifts table one more time
 --
 
-
+SELECT * FROM gifts;
 
 -- BONUSES
 
@@ -110,16 +110,16 @@ RETURNING *;
  \echo Count the total number of gifts that have the word candle in it
 -- 
 
-
+SELECT COUNT(gift) as candle_gifts FROM gifts WHERE gift ILIKE 'candle';
 --
 \echo Get the AVEREAGE value from all the gifts
 --
-
+SELECT ROUND(AVG(value), 2)::money as average_value FROM gifts;
 
 -- 
  \echo Limit to 3 gifts, offset by 2 and order by price descending
 --
-
+SELECT * FROM gifts ORDER BY value DESC OFFSET 2;
 --
 -- finish
 --
