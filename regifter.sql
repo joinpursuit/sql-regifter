f2853394ee7776ef98ba0c7a44385346c9708903
Candice Agard <candiceagard@Candices-MacBook-Pro.local>
2021-07-02 16:08:18 -0400

    Database completed

--- regifter.sql
+++ regifter.sql
@@ -15,93 +15,101 @@ CREATE DATABASE regifter;
 -- giver - string
 -- value - integer
 -- previously_regifted boolean
+CREATE TABLE gifts (id SERIAL PRIMARY KEY, gift TEXT, giver TEXT, value INTEGER, prviously_regifted BOOLEAN);
 
-
--- 
+--
 \echo See details of the table you created
--- 
-
-
--- 
-\echo Alter the table so that the column price is changed to value 
--- 
-
-
--- 
-\echo Insert a peach candle, given by 'Santa' thats value is 9 and has been previously regifted
--- 
+--
+\d gifts
 
 
 --
+\echo Alter the table so that the column price is changed to value
+--
+-- This table was already created to have value as one of the columns. I altered the column to have price and then reverted it to value.
+ALTER TABLE gifts RENAME value TO price;
+ALTER TABLE gifts RENAME price TO value;
+
+
+
+--
+\echo Insert a peach candle, given by 'Santa' thats value is 9 and has been previously regifted
+--
+INSERT INTO gifts (gift, giver, value, previously_regifted) VALUES ('peach candle', 'Santa', '9', TRUE);
+
+--
 \echo Query for all the columns in your gifts table
--- 
+--
 
 
 --
 \echo Uncomment below to insert 5 more gifts
--- 
-
--- INSERT INTO gifts (gift, giver, value, previously_regifted)
--- VALUES
--- ('peach candle', 'Santa', '9', TRUE),
--- ('cinnamon candle', 'Nick', '19', TRUE),
--- ('soap on a rope', 'Rudolf', '29', FALSE),
--- ('potpurri', 'Elf on the Shelf', '39', TRUE),
--- ('mango candle', 'The Boss', '49', FALSE)
--- ;
-
--- 
-\echo Insert 5 more gifts of your own choosing,  include 1 more candle
 --
 
+ INSERT INTO gifts (gift, giver, value, previously_regifted)
+ VALUES ('cinnamon candle', 'Nick', '19', TRUE),
+ ('soap on a rope', 'Rudolf', '29', FALSE),
+ ('potpurri', 'Elf on the Shelf', '39', TRUE),
+ ('mango candle', 'The Boss', '49', FALSE);
+
+--
+\echo Insert 5 more gifts of your own choosing,  include 1 more candle
+--
+INSERT INTO gifts (gift, giver, value, previously_regifted)
+VALUES ('bath robe', 'Samantha', '15', FALSE),
+('video games', 'Chris', '60', FALSE),
+('bath and bodyworks candle', 'Cupid', '20', FALSE),
+('baking dish', 'Nancy', '10', TRUE),
+('big bang theory blueray', 'Santa', '25', TRUE);
 
 
 --
 \echo Query for gifts with a price greater than or equal to 20
 --
-
+SELECT * FROM gifts WHERE value >= 20;
 
 --
 \echo Query for every gift that has the word candle in it, only show the gift column
 --
-
+SELECT gift FROM gifts WHERE gift LIKE '%candle%';
 
 --
 \echo Query for every gift whose giver is Santa OR value is greater than 30
 --
-
+SELECT * FROM gifts WHERE giver = 'Santa' OR value > 30;
 
 --
 \echo Query for every gift whose giver is NOT Santa
 --
-
+SELECT * FROM gifts WHERE giver != 'Santa';
 
 --
 \echo Update the second gift to have a value of 2999
--- 
-
+--
+UPDATE gifts SET value = '2999' WHERE id = 2;
 
 --
 \echo Query for the updated item
 --
+SELECT value FROM gifts WHERE id = 2;
 
 
 --
 \echo Delete all the gifts from Santa and return the 'value' and 'gift' of the gift you have deleted
 --
-
+DELETE FROM gifts WHERE giver = 'Santa' RETURNING 'value', 'gift';
 
 --
 \echo Query for all the columns in your gifts table one more time
 --
-
+SELECT * FROM gifts;
 
 
 -- BONUSES
 
 --
  \echo Count the total number of gifts that have the word candle in it
--- 
+--
 
 
 --
@@ -109,7 +117,7 @@ CREATE DATABASE regifter;
 --
 
 
--- 
+--
  \echo Limit to 3 gifts, offset by 2 and order by price descending
 --
 
