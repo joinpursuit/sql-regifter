fbfe738cbb3d5bf76fe22b88497102a38d2eef13
Obaid Rustemi <orustemi@gmail.com>
2021-07-02 13:24:24 -0400

    add query for gift prices above 20

--- regifter.sql
+++ regifter.sql
@@ -44,26 +44,31 @@ INSERT INTO gifts (gift,giver,price,previously_regifted) VALUES ('peach candle',
 \echo Uncomment below to insert 5 more gifts
 -- 
 
--- INSERT INTO gifts (gift, giver, value, previously_regifted)
--- VALUES
--- ('peach candle', 'Santa', '9', TRUE),
--- ('cinnamon candle', 'Nick', '19', TRUE),
--- ('soap on a rope', 'Rudolf', '29', FALSE),
--- ('potpurri', 'Elf on the Shelf', '39', TRUE),
--- ('mango candle', 'The Boss', '49', FALSE)
--- ;
+INSERT INTO gifts (gift, giver, price, previously_regifted)
+VALUES
+('peach candle', 'Santa', '9', TRUE),
+('cinnamon candle', 'Nick', '19', TRUE),
+('soap on a rope', 'Rudolf', '29', FALSE),
+('potpurri', 'Elf on the Shelf', '39', TRUE),
+('mango candle', 'The Boss', '49', FALSE);
 
 -- 
 \echo Insert 5 more gifts of your own choosing,  include 1 more candle
 --
 
-
+INSERT INTO gifts (gift, giver, price, previously_regifted)
+VALUES
+('strawberry candle', 'Santa', '9', TRUE),
+('PS5', 'Russ', '499', FALSE),
+('cozy coupe', 'Baleigh', '60', FALSE),
+('Best Buy giftcard', 'toys for tots', '200', FALSE),
+('apple watch 6', 'Sofia', '600', FALSE);
 
 --
 \echo Query for gifts with a price greater than or equal to 20
 --
 
-
+SELECT * FROM gifts where price >= 20;
 --
 \echo Query for every gift that has the word candle in it, only show the gift column
 --
