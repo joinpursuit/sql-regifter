b21c6c142924d623dbf059b59912b50d0c43a308
Rohan-Sterling <rohansterling@pursuit.org>
2022-01-27 17:43:51 -0500

    fix spelling, rename price to value

--- regifter.sql
+++ regifter.sql
@@ -19,7 +19,7 @@ CREATE TABLE gifts(
     id SERIAL PRIMARY KEY, 
     gift TEXT, 
     giver TEXT, 
-    price INT, perviously_regifted boolean
+    price INT, previously_regifted BOOLEAN
 );
 
 
@@ -31,9 +31,8 @@ CREATE TABLE gifts(
 -- 
 \echo Alter the table so that the column price is changed to value 
 -- 
-ALTER TABLE gifts(
-
-)
+ALTER TABLE gifts
+RENAME price TO value;
 
 -- 
 \echo Insert a peach candle, given by 'Santa' thats value is 9 and has been previously regifted
