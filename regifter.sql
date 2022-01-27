fe8951f73ebf7f9e01c342436799331731845e6a
salrey <tekdehn@gmail.com>
2022-01-27 12:31:29 -0500

    Altered table value to price, inserted Santa's gift

--- regifter.sql
+++ regifter.sql
@@ -23,21 +23,26 @@ CREATE TABLE
 -- 
 \echo See details of the table you created
 -- 
-\d gifts;
+-- \d gifts;
 
 -- 
 \echo Alter the table so that the column price is changed to value 
 -- 
-
+ALTER TABLE gifts RENAME value TO price;
+-- \d gifts
 
 -- 
 \echo Insert a peach candle, given by 'Santa' thats value is 9 and has been previously regifted
 -- 
-
+INSERT INTO 
+    gifts (gift, giver, price, previously_regifted)
+VALUES 
+    ('Peach', 'Santa', 9, TRUE);
 
 --
 \echo Query for all the columns in your gifts table
 -- 
+SELECT * FROM gifts;
 
 
 --
