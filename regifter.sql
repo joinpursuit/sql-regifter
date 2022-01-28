c7a7699713a7bb749ba815599a3fde43f5d16f90
Mukayila ALAO <mukayilaalao@pursuit.org>
2022-01-28 13:03:05 -0500

    done with bonus

--- regifter.sql
+++ regifter.sql
@@ -116,17 +116,17 @@ SELECT * FROM gifts;
 --
  \echo Count the total number of gifts that have the word candle in it
 -- 
-
+SELECT COUNT(gift) FROM gifts WHERE gift LIKE '%candle%';
 
 --
 \echo Get the AVEREAGE value from all the gifts
 --
-
+SELECT AVG(price) FROM gifts;
 
 -- 
  \echo Limit to 3 gifts, offset by 2 and order by price descending
 --
-
+SELECT * FROM gifts ORDER BY price DESC LIMIT 3 OFFSET 2;
 --
 -- finish
 --
