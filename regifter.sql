ef03df4edb084fa859fe8fc788520aaacfa39a00
Lili Huang <86433459+LILYHUANGPURSUIT@users.noreply.github.com>
2022-01-29 10:45:20 -0500

    Update regifter.sql

--- regifter.sql
+++ regifter.sql
@@ -134,7 +134,7 @@ SELECT AVG(value) FROM gifts;
 -- 
  \echo Limit to 3 gifts, offset by 2 and order by price descending
 --
-SELECT * FROM gifts LIMIT 3 OFFSET 2;
+SELECT * FROM gifts ORDER BY value DESC LIMIT 3 OFFSET 2;
 
 --
 -- finish
