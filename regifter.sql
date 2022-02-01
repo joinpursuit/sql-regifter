887d0fb7a170e6b6b6aeb99efffed567cfdd76cf
Charlie Moran <Carlosamoran18@gmail.com>
2022-01-31 20:26:19 -0500

    Alter Gifts done

--- regifter.sql
+++ regifter.sql
@@ -26,16 +26,29 @@ CREATE TABLE gifts(
 \echo See details of the table you created
 -- 
 
+\dt
 
 -- 
 \echo Alter the table so that the column price is changed to value 
 -- 
-
+ALTER TABLE gifts
+RENAME price TO value;
 
 -- 
 \echo Insert a peach candle, given by 'Santa' thats value is 9 and has been previously regifted
 -- 
 
+INSERT INTO gifts (
+    gift,
+    giver,
+    value,
+    previously_regifted,
+) VALUES (
+    "a peach candle",
+    "Santa",
+    9,
+    TRUE,
+)
 
 --
 \echo Query for all the columns in your gifts table
