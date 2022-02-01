0237508d87346d9d1a844ff13b08e518f0703877
TheodoreBFrazier <theo.frazier@icloud.com>
2022-01-31 22:49:01 -0500

    Delete from Santa and return value work

--- regifter.sql
+++ regifter.sql
@@ -122,6 +122,7 @@ SELECT * FROM gifts WHERE id = 2;
 \echo Delete all the gifts from Santa and return the 'value' and 'gift' of the gift you have deleted
 --
 
+DELETE FROM gifts WHERE giver = 'Santa' RETURNING price, gift;
 
 --
 \echo Query for all the columns in your gifts table one more time
