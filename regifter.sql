31ef664940cf7f35c716ea6c406c44cccca933dc
Charlie Moran <Carlosamoran18@gmail.com>
2022-01-31 20:23:11 -0500

    Table done

--- regifter.sql
+++ regifter.sql
@@ -15,7 +15,12 @@ CREATE DATABASE regifter;
 -- giver - string
 -- value - integer
 -- previously_regifted boolean
-
+CREATE TABLE gifts(
+    id SERIAL PRIMARY KEY,
+    gift TEXT,
+    giver TEXT,
+    price INT, previously_regifted BOOLEAN,
+)
 
 -- 
 \echo See details of the table you created
